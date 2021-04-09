04a989bb432c88d8c52110eaa7cd7ff120eddd79
vipul soni <soni.vipul697@gmail.com>
2021-04-09 19:44:11 +0530

    item

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+function Button()
+{
+    return(
+        <div className="button">
+
+        </div>
+    );
+}
+export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,25 @@
 import React from "react"
 import './Header.css'
 import TeslaLogo from '../assets/teslaLogoSmall.svg'
-function Header()
-{
+function Header() {
     return (
-        <div className= "header">
-<div className="header__logo">
-<img src={TeslaLogo} alt="Tesla Logo"/>
-</div>
-<div className="header__center">
-    <p>Model S</p>
-    <p>Model 3</p>
-    <p>Model X</p>
-    <p>Model Y</p>
-    <p>Model Roof</p>
-    <p>Model Panel</p>
-</div>
+        <div className="header">
+            <div className="header__logo">
+                <img src={TeslaLogo} alt="Tesla Logo" />
+            </div>
+            <div className="header__center">
+                <p>Model S</p>
+                <p>Model 3</p>
+                <p>Model X</p>
+                <p>Model Y</p>
+                <p>Model Roof</p>
+                <p>Model Panel</p>
+            </div>
 
-<div className="header__right">
-    <p>Shop</p>
-    <p>Tesla Account</p>
-</div>
+            <div className="header__right">
+                <p>Shop</p>
+                <p>Tesla Account</p>
+            </div>
         </div>
 
     );
